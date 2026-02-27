a8e7a625c452f1a7036bd5a3d4b4c9350278764f
Web Design Etobicoke <webdesignetobicoke@gmail.com>
2026-02-27 00:40:45 -0500

    Keep only G-W6SK4TZ4C9 Google Analytics property

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,13 +13,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics - Main Property */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JTJ1GMJDCZ" />
-        <Script id="google-analytics-main">
+        {/* Google Analytics */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-W6SK4TZ4C9" />
+        <Script id="google-analytics">
           {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-JTJ1GMJDCZ');`}
+          gtag('config', 'G-W6SK4TZ4C9');`}
         </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -30,22 +30,6 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5ZR653T8');`}
-        </Script>
-        {/* Google Analytics - Secondary Property */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5JG2WE35WF" />
-        <Script id="google-analytics-secondary">
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-5JG2WE35WF');`}
-        </Script>
-        {/* Google Analytics - Third Property */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-W6SK4TZ4C9" />
-        <Script id="google-analytics-tertiary">
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-W6SK4TZ4C9');`}
         </Script>
       </head>
       <body className="antialiased">

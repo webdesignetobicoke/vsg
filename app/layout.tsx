f1e1c224a8e5d88d6fbb4012a9d66c11d834f144
Web Design Etobicoke <webdesignetobicoke@gmail.com>
2026-02-27 00:10:51 -0500

    Add Google Tag Manager (GTM-5ZR653T8)

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5ZR653T8');`}
+        </Script>
         {/* Google Analytics */}
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5JG2WE35WF" />
         <Script id="google-analytics">
